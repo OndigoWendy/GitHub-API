c683d10406f6136f0a9c87d136bdd5784cfcd202
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-29 03:29:06 +0300

    Create an event emmiter for a search option and ingect it as a dependency to the landing-page component.

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  
+  //public userQuery!: string;
+  public githubUserQuery: string = 'Enter username here';
+  @Output() searchResult = new EventEmitter<any>()
+  constructor() { }
+
+  searchUser(){
+    this.searchResult.emit(this.githubUserQuery);
+  }
   ngOnInit(): void {
   }
-
 }
